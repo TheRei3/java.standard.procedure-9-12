604952993e6b92eb1d31ee3fb98a6eaf26ecddf3
admin <admin@host.docker.internal>
2021-10-11 08:27:44 +0900

    ex9no12

---
+++ ex9no12/src/ex9no12/ex9no12.java
@@ -0,0 +1,11 @@
+package ex9no12;
+
+public class ex9no12 {  //  466p
+	public static void main(String[] args) {
+		String s1 = "AAA";
+		String s2 = "AAA";
+		String s3 = "AAA";
+		String s4 = "BBB";
+	}
+
+}
